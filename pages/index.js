0f0ccd7a4da7c92a3a2f6a3fa5c5b733ddefe1fa
huaying <royal3501@gmail.com>
2020-03-18 01:08:19 +0800

    test connecting api with frontend

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
-export default function Index() {
+import fetch from "isomorphic-unfetch";
+
+export default function Index({ data }) {
   return (
     <div>
       <p>Hello Next.js</p>
+      <div>{data.cases}</div>
     </div>
   );
 }
+
+Index.getInitialProps = async function() {
+  const res = await fetch("http://localhost:5001/all/");
+  const data = await res.json();
+
+  console.log(data);
+
+  return {
+    data
+  };
+};
