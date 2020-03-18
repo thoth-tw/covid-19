41f66c476f9bc0a920ad0e9d6aad3c160afaa93b
huaying <royal3501@gmail.com>
2020-03-19 03:06:28 +0800

    style news

--- components/news.js
+++ components/news.js
@@ -34,6 +34,7 @@ export default function News({ news }) {
           width: 120px;
           height: 80px;
           object-fit: cover;
+          filter: grayscale(100%);
         }
         .content {
           margin-left: 20px;
@@ -48,6 +49,18 @@ export default function News({ news }) {
           margin-top: 5px;
           color: #585858;
           letter-spacing: 1px;
+        }
+        @media (max-width: 500px) {
+          .item {
+            flex-direction: column;
+          }
+          .img img {
+            width: 100%;
+            height: 200px;
+          }
+          .content {
+            margin-left: 0;
+          }
         }
       `}</style>
     </Card>
